33701c7278ebc2043022fbdb18c2bfa03d23d0e7
3jasonrodriguez <3jasonrodriguez@gmail.com>
2026-03-07 22:12:56 -0500

    Search playlists

--- etl/ingestion/search_playlists.py
+++ etl/ingestion/search_playlists.py
@@ -5,7 +5,7 @@ from requests.exceptions import RequestException, HTTPError
 
 def spotify_searches(search_list, search_type, limit):
     all_results = []
-    type_map = {"playlist":"playlists", "artist":"artists", "track":"tracks"}
+    type_map = {"playlist":"playlists", "artist":"artists", "track":"tracks", "album":"albums"}
     token = get_spotify_access_token()
     search_url = "https://api.spotify.com/v1/search"
     headers = {
@@ -13,19 +13,43 @@ def spotify_searches(search_list, search_type, limit):
         "Accept": "application/json" # Optional: specify expected response format
     }
     for query in search_list:
-        payload = {'q':query, 'type':search_type, 'limit':limit} 
-        search_response = requests.get(search_url, headers=headers, params=payload)
-        data = search_response.json()
+        payload = {
+                'q':query, 
+                'type':search_type, 
+                'limit':limit
+        } 
+        search_response = requests.get(search_url, headers=headers, params=payload, timeout=10)
+        try:
+            search_response.raise_for_status()
+        except HTTPError as e:
+            print(f"HTTP Error: {e}")
+            continue
+        except RequestException as e:
+            print(f"Request failed: {e}")
+            continue
+        try:
+            data = search_response.json()
+        except (ValueError, KeyError) as e:
+            print(f"Failed to parse JSON for query '{query}': {e}")
+            continue
+        print(f"result for {query}")
+        #items = print(data[(type_map[search_type])]['items'])
+        items = data.get(type_map[search_type], {}).get("items", [])
+        print(f"Query: {query} -> {len(items)} results")
+        #print(items)
         #Append items to master list
         all_results.extend(items)
-
-    return all_results
+    # Deduped search results
+    unique_results = {p["id"]: p for p in all_results}
+    print(f"Total results: {len(all_results)}")
+    print(f"Unique results: {len(unique_results)}")
+    print(list(unique_results.values()))
+    return list(unique_results.values())
 
 
 #Define main for reusability and importing
 def main():
     genre_list = ["prog rock", "progressive rock", "math rock"]
-    searches = spotify_searches(genre_list, "playlist")
-    print(len(searches))
+    searches = spotify_searches(genre_list, "playlist", 1)
 if __name__ == "__main__":
     main()
