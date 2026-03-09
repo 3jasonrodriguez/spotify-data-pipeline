6a3e37edd570d3c6b228a8bab68f9cbceb14dc16
3jasonrodriguez <3jasonrodriguez@gmail.com>
2026-03-08 20:11:14 -0400

    loaded playlist search results to s3

---
+++ etl/ingestion/load_to_s3.py
@@ -0,0 +1,51 @@
+import os
+import json
+from dotenv import load_dotenv
+from datetime import datetime
+from etl.ingestion.search_playlists import spotify_searches
+import boto3
+from botocore.exceptions import ClientError
+from botocore.exceptions import NoCredentialsError
+#Load playlists seacrh results to s3
+def load_to_s3(results_list, entity_type):
+    #lines for loading JSONL to s3
+    lines = []
+    #Grab date partials to build out s3 bucket/partition structure
+    today = datetime.now()
+    year = today.strftime("%Y")
+    month = today.strftime("%m")
+    day = today.strftime("%d")
+    s3_key = f"raw/{entity_type}/year={year}/month={month}/day={day}/{entity_type}.jsonl"
+    #Iterate through each result dictionary
+    for result in results_list:
+        #Add each result to a the list
+        lines.append(json.dumps(result))
+    #join resulting lines to jsonl string
+    jsonl_string = "\n".join(lines)
+    print(jsonl_string)
+    #Using creds to for s3 client connect
+    client = boto3.client(
+        "s3",
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        region_name=os.getenv("AWS_REGION")
+    )
+    #Placing object to s3 bucket
+    try:
+        client.put_object(
+            Bucket=os.getenv("S3_BUCKET_NAME"),
+            Key=s3_key,
+            Body=jsonl_string.encode("utf-8") # S3 expects bytes, so the jsonl string has to be encoded
+        )
+    except NoCredentialsError:
+        print("AWS credentials not found - check your .env file")
+    except ClientError as e:
+        print(f"S3 upload failed: {e}")
+
+
+def main():
+    genre_list = ["prog rock", "progressive rock", "math rock"]
+    searches = spotify_searches(genre_list, "playlist", 10)
+    load = load_to_s3(searches, "playlist")
+if __name__ == "__main__":
+    main()
