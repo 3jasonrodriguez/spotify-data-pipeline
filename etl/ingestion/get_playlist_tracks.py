86d884727dc14e6598397413d55bb3b73438e49d
3jasonrodriguez <3jasonrodriguez@gmail.com>
2026-03-14 14:34:56 -0400

    grabbing latest part from s3

---
+++ etl/ingestion/get_playlist_tracks.py
@@ -0,0 +1,72 @@
+import os
+import json
+from dotenv import load_dotenv
+from datetime import datetime
+from etl.utils.spotify_auth import get_spotify_access_token
+import requests
+from requests.exceptions import RequestException, HTTPError
+import boto3
+from botocore.exceptions import ClientError
+from botocore.exceptions import NoCredentialsError
+
+def get_playlist_tracks():
+    #token = get_spotify_access_token()
+    load_dotenv()
+    #Using creds to for s3 client connect
+    client = boto3.client(
+        "s3",
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        region_name=os.getenv("AWS_REGION")
+    )
+    #Grabbing latest partition in S3
+    try:
+        partitions = client.list_objects_v2(
+            Bucket=os.getenv("S3_BUCKET_NAME"),
+            Prefix="raw/playlist/",
+        )
+    except NoCredentialsError:
+        print("AWS credentials not found - check your .env file")
+    except ClientError as e:
+        print(f"S3 data pull failed: {e}")
+    #Grab S3 contents containing the keys within the prefix
+    partition_contents = partitions.get("Contents", [])
+    if not partition_contents:
+        print("No partitions found in S3")
+        return
+   #Used to grab all yyyymmdd extracted values to determine the latest
+    part_list = []
+    #Iterate over all the keys to grab the key paths
+    for part in partition_contents:
+        filename = part["Key"]
+        year = filename.split("year=")[1].split("/")[0]
+        month = filename.split("month=")[1].split("/")[0]
+        day = filename.split("day=")[1].split("/")[0]
+        dt = f"{year}{month}{day}"
+        part_list.append(dt)
+    #Grab latest partition and make it a datetime object to parse out the year/month/day
+    latest_part = max(part_list)
+    try:
+        playlist_file = client.get_object(
+            Bucket=os.getenv("S3_BUCKET_NAME"),
+            Key = f"raw/playlist/year={latest_part[:4]}/month={latest_part[4:6]}/day={latest_part[6:]}/playlist.jsonl"
+        )
+    except NoCredentialsError:
+        print("AWS credentials not found - check your .env file")
+    except ClientError as e:
+        print(f"S3 data pull failed: {e}")
+    #Contents from s3 pull
+    playlists_contents = playlist_file["Body"].read().decode("utf-8")
+    #Splits the jsonl string into a list of lines, removes trailing newlines, skips empty lines, parses each line into a dict 
+    playlists = [json.loads(line) for line in playlists_contents.strip().splitlines() if line]
+    #Grab playlist ids
+    playlist_ids = [p["id"] for p in playlists if p and p.get("id")]
+    #For each playlist, grab the tracks
+    for id in playlist_ids:
+        items_url = f"https://api.spotify.com/v1/playlists/{id}/items"
+        print(items_url)
+    
+def main():
+    playlist_tracks = get_playlist_tracks()
+if __name__ == "__main__":
+    main()
