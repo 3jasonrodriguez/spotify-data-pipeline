bc7b697ed57393bf7012a8f87ddb7857c0a2ff1f
3jasonrodriguez <3jasonrodriguez@gmail.com>
2026-03-18 22:31:10 -0400

    loaded all streaming history to s3 by year partitions

--- etl/ingestion/ingest_streaming_history.py
+++ etl/ingestion/ingest_streaming_history.py
@@ -2,17 +2,44 @@ import yaml
 import os
 import json 
 from datetime import datetime
-with open("config.yaml", "r") as f:
-    config = yaml.safe_load(f)
+from etl.ingestion.load_to_s3 import load_to_s3
 
-path = config["paths"]["streaming_history"]
-files = os.listdir(path)
-json_files = [f for f in os.listdir(path) if f.endswith(".json")]
 
-for filename in json_files:
-    full_path = os.path.join(path, filename)
-    with open(full_path, "r", encoding="utf-8") as f:
-        records = json.load(f)
-for record in records:
-    year = datetime.strptime(record.get('ts'), "%Y-%m-%dT%H:%M:%SZ").year
-    print(year)
+def ingest_streaming_history():
+    #Loading of config file with path to streaming history
+    with open("config.yaml", "r") as f:
+        config = yaml.safe_load(f)
+    #Loading the files from the paths
+    path = config["paths"]["streaming_history"]
+    files = os.listdir(path)
+    #Only grab the audio files (json)
+    json_files = [f for f in os.listdir(path) if f.endswith(".json")]
+    #Using a dict to put each stream by year for later partitioning
+    records_dict_year = {}
+    #Fields removed from the spotify json records
+    fields_to_remove = ['ip_addr', 'episode_name','episode_show_name','spotify_episode_uri', 'audiobook_title', 'audiobook_uri','audiobook_chapter_uri', 'audiobook_chapter_title']
+    #Loop through all files
+    for filename in json_files:
+        full_path = os.path.join(path, filename)
+        with open(full_path, "r", encoding="utf-8") as f:
+            records = json.load(f)
+            for record in records:
+                #Skip non audio streams
+                if record.get('spotify_track_uri') is None:
+                    continue
+                filtered_record = {key: value for key, value in record.items() if key not in fields_to_remove}
+                #Extract the year of the timestamp
+                year = str(datetime.strptime(filtered_record.get('ts'), "%Y-%m-%dT%H:%M:%SZ").year)
+                #Place record into the year key
+                if year in records_dict_year:
+                    records_dict_year[year].append(filtered_record)
+                else:
+                    records_dict_year[year] = [filtered_record]
+    return records_dict_year
+            
+def main():
+    records_by_year = ingest_streaming_history()
+    for year, records in records_by_year.items():
+        load_to_s3(records, "streaming_history", year)
+if __name__ == "__main__":
+    main()
--- etl/ingestion/load_to_s3.py
+++ etl/ingestion/load_to_s3.py
@@ -9,18 +9,23 @@ import boto3
 from botocore.exceptions import ClientError
 from botocore.exceptions import NoCredentialsError
 #Load playlists seacrh results to s3
-def load_to_s3(results_list, entity_type):
+def load_to_s3(results_list, entity_type, year=None):
     #Load .env
     load_dotenv()
+    #If a year is sent (for streaming data), use the yearly partition
+    if year:
+        s3_key = f"raw/{entity_type}/year={year}/{entity_type}.jsonl"
+    else:
+        #Build out the s3 key for todays date
+        #Grab date partials to build out s3 bucket/partition structure
+        today = datetime.now()
+        year = today.strftime("%Y")
+        month = today.strftime("%m")
+        day = today.strftime("%d")
+        s3_key = f"raw/{entity_type}/year={year}/month={month}/day={day}/{entity_type}.jsonl"
+    #Iterate through each result dictionary
     #lines for loading JSONL to s3
     lines = []
-    #Grab date partials to build out s3 bucket/partition structure
-    today = datetime.now()
-    year = today.strftime("%Y")
-    month = today.strftime("%m")
-    day = today.strftime("%d")
-    s3_key = f"raw/{entity_type}/year={year}/month={month}/day={day}/{entity_type}.jsonl"
-    #Iterate through each result dictionary
     for result in results_list:
         #Add each result to a the list
         lines.append(json.dumps(result))
@@ -40,6 +45,7 @@ def load_to_s3(results_list, entity_type):
             Key=s3_key,
             Body=jsonl_string.encode("utf-8") # S3 expects bytes, so the jsonl string has to be encoded
         )
+        print("{s3_key} loaded to S3")
     except NoCredentialsError:
         print("AWS credentials not found - check your .env file")
     except ClientError as e:
@@ -52,8 +58,8 @@ def main():
     #load_to_s3(searches, "playlists")
     #saved_tracks = get_saved_tracks()
     #load_to_s3(saved_tracks, "saved_tracks")
-    art_genres = get_artists_genres()
-    load_to_s3(art_genres, "artists")
-
+    #art_genres = get_artists_genres()
+    #load_to_s3(art_genres, "artists")
+    print()
 if __name__ == "__main__":
     main()
